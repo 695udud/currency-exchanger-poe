e5167025f068015aff866c2e069a809c53be99d8
695udud <demelo.luizeduardo@gmail.com>
2025-11-18 08:25:55 -0300

    Create input.jsx

---
+++ src/components/ui/input.jsx
@@ -0,0 +1,8 @@
+export default function Input(props) {
+  return (
+    <input
+      {...props}
+      style={{ padding: "6px 10px", margin: "6px 0", width: "100%", borderRadius: 6, border: "1px solid #aaa" }}
+    />
+  );
+}
