aca6cd8f7d17b7f8c8908dab2bd09f9772fa5196
695udud <demelo.luizeduardo@gmail.com>
2025-11-18 08:28:23 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/meu-sistema/", // ALTERE para o nome do repositório no GitHub
+  base: "/currency-exchanger-poe/", 
 });
